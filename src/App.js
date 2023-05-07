22dd44a4fa8cd79f673b1d037a231ba1028a2ceb
Basu Thapa <thapabasu000@gmail.com>
2023-05-07 07:36:07 +0545

    making responsive

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom';
 import Home from './components/pages/Home';
 function App() {
   return (
-    <>
+    <div className='page-wrapper'>
     <Router>
       <Navbar />
       <Routes>
       <Route path="/" element={<Home />} /> 
       </Routes>
     </Router>
-    </>
+    </div>
    
 
   );
